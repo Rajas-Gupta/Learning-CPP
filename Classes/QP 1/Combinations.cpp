10b9c181365fa1ca2256da700ab8e9e8481a11ce
Rajas Gupta <rajasgupta2704@gmail.com>
2022-09-09 10:54:49 +0530

    QPS

---
+++ Classes/QP 1/Combinations.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int kd;
+void solve(int n,int k,int * out,int j,int s){
+    if (k==0)
+    {
+        for (int i = 0; i < kd; i++)
+        {
+            cout<<out[i]<<" ";
+        }
+        cout<<endl;
+    }
+    for (int i = s; i <= n; i++)
+    {
+        out[j]=i;
+        solve(n,k-1,out,j+1,i+1);
+    }
+    
+}
+int main()
+{
+    int n=4;
+    int k=2;
+    int out[100];
+    kd=k;
+    solve(n,k,out,0,1);
+    return 0;
+}
