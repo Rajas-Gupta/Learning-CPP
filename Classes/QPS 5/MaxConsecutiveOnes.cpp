81270c72eb2c1a5644aff6dace0be9846772f0ff
Rajas Gupta <rajasgupta2704@gmail.com>
2022-11-08 13:00:55 +0530

    QPS 5

---
+++ Classes/QPS 5/MaxConsecutiveOnes.cpp
@@ -0,0 +1,8 @@
+// MaxConsecutiveOnes.cpp
+// https://leetcode.com/problems/max-consecutive-ones-iii/
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+    }
+};
