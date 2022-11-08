2e5fe3b33494a7876ef39fcc291335b7a11b0675
Rajas Gupta <rajasgupta2704@gmail.com>
2022-11-08 13:01:01 +0530

    QPS 5

--- Classes/QPS 5/MaxConsecutiveOnes.cpp
+++ Classes/QPS 5/MaxConsecutiveOnes.cpp
@@ -3,6 +3,26 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        
+        int n,i,j,flips,ans;
+        n=nums.size();
+        i=j=flips=ans=0;
+        while (j<n)
+        {
+            if (nums[j]==0)
+            {
+                flips++;
+            }
+            while (flips>k)
+            {
+                if (nums[i]==0)
+                {
+                    flips--;
+                }
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        return ans;
     }
 };
